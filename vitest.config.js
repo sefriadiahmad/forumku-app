c5f6dbaeca0d2cdc8c643b1c23daf1ff8f892446
Ahmad Sefriadi <sefriadiahmad@gmail.com>
2026-08-07 19:32:58 +0700

    update compatibility issues between jsdom and undici

--- vitest.config.js
+++ vitest.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
     environment: 'jsdom',
     setupFiles: './src/setupTests.js',
     include: ['src/**/*.{test,spec}.{js,mjs,cjs,jsx,mjsx}'],
+    pool: 'forks',
     coverage: {
       provider: 'v8',
       reporter: ['text', 'json', 'html'],
